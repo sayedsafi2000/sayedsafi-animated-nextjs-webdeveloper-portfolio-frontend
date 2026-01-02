8532635aecb8afe57ef50cf47b6b0ef57f33e47d
Sayed MD Safiuddin Safi <safiuddin102030@gmail.com>
2026-01-03 02:09:34 +0600

    added SMTP

--- next.config.js
+++ next.config.js
@@ -20,6 +20,8 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 31536000, // 1 year
     unoptimized: false,
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: 'https',
